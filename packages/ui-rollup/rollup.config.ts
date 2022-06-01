dbb55f7f2870d251d9f71d0dea1697e357911e3e
riccardoperra <riccardo.perra@icloud.com>
2022-06-01 21:02:15 +0200

    add rollup vanilla extract config

--- packages/ui-rollup/rollup.config.ts
+++ packages/ui-rollup/rollup.config.ts
@@ -1,18 +1,36 @@
 import withSolid from "rollup-preset-solid";
+import { vanillaExtractPlugin } from "@vanilla-extract/rollup-plugin";
+import { dependencies, peerDependencies } from "./package.json";
+
+const externals = [
+  ...Object.keys(peerDependencies),
+  ...Object.keys(dependencies),
+  "solid-js/web",
+  "solid-js/store",
+  "@vanilla-extract/recipes/createRuntimeFn",
+];
 
 export default withSolid({
   input: "src/index.tsx",
   targets: ["esm", "cjs"],
+  plugins: [vanillaExtractPlugin()],
+  external: externals,
   output: [
     {
       preserveModules: true,
       entryFileNames: "[name].js",
+      assetFileNames({ name }) {
+        return name?.replace(/^src\//, "") ?? "";
+      },
       dir: "./dist/esm",
       format: "esm",
     },
     {
       preserveModules: true,
       entryFileNames: "[name].js",
+      assetFileNames({ name }) {
+        return name?.replace(/^src\//, "") ?? "";
+      },
       dir: "./dist/cjs",
       format: "cjs",
     },
